2725d24c8561082f52f5da5dfca6e0e9207afd13
RomanKozar <kozarrma2004@gmail.com>
2024-05-01 23:46:00 +0300

    adding api key, weather filling, weather indicator, completing postman requests

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+
+const api_key = "6641537634a36928950ce352fa6a464c";
+
+/**
+ * Fetches data from server.
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  /**
+   * @param {string} query Search query London, New York
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
 
 const Api = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Api
+export default Api;
